97d75f7b69242d746b245baf92af125e49bfabbb
Ren Hanamura <74428701+RenHanamura@users.noreply.github.com>
2022-03-01 04:22:10 -0600

    What a shame

---
+++ Behind the scenes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evidencia_Unidad_1
+{
+    public class Artistas
+    {
+        public string NombreA { get; set; }
+        public string Genero { get; set; }
+        public string Albums { get; set; }
+        public string CanRec { get; set; }
+
+        public Artistas(string NombreArt, string Gen, string Album, string CanciRecom)
+        {
+            NombreA = NombreArt;
+            Genero = Gen;
+            Albums = Album;
+            CanRec = CanciRecom;
+        }
+
+        public Artistas() { }
+
+       public class MoarData : Artistas
+        {
+            public string Streaming;
+            public string SM;
+
+            public MoarData(string NombreArt, string Gen, string Album, string CanciRecom, string Plat, string Net) : base(NombreArt, Gen, Album, CanciRecom)
+            {
+                Streaming = Plat;
+                SM = Net;
+            }
+        }
+    }
+   
+}
---
+++ E1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static Evidencia_Unidad_1.Artistas;
+
+namespace Evidencia_Unidad_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string elec;
+            Console.WriteLine("Bienvenido a este programa el cual te dira algunos de mis artistas favoritos, por favor selecciona uno:\n1. Joji\n2. DEMONDICE\n3. half·alive");
+            elec = Console.ReadLine();
+
+            switch (elec)
+            {
+                case "1":
+                    {
+                        MoarData Joji = new MoarData("Joji", "R&B/Soul", "In Tongues, Ballads 1, Nectar", "Slow Dancing in the dark, Sanctuary, Test Drive, Wanted U, Yeah Right", "Spotify, Youtube Music, Apple Music, Deezer", "\nTwitter: @sushitrash, Instagram: @sushitrash, Facebook: @jojikansai");
+                        Console.WriteLine("Artista: {0}\nGenero: {1}\nAlbums: {2}\nCanciones recomendadas: {3}\nLo encontraras en: {4}\nSus redes son: {5}", Joji.NombreA, Joji.Genero, Joji.Albums, Joji.CanRec, Joji.Streaming, Joji.SM);
+                        break;
+                    }
+                case "2":
+                    {
+                        MoarData DD = new MoarData("DEMONDICE", "Hip-hop/Rap", "DEMONDICE, The Red, White and Black, American Saikoro, Kakigori Galaxy Astronout, Strange Fiction, Alkatraz", "Soundless, Gang Gang, Kawaii!!, Wannabe, Alt-er-native!!, Legendary Stray, Words That I Could Say to You", "Spotify, Youtube Music, Apple Music, Deezer", "\nTwitter: @DEMONDICEKaren");
+                        Console.WriteLine("Artista: {0}\nGenero: {1}\nAlbums: {2}\nCanciones recomendadas: {3}\nLa encontraras en: {4}\nSus redes son: {5}", DD.NombreA, DD.Genero, DD.Albums, DD.CanRec, DD.Streaming, DD.SM);
+                        break;
+                    }
+                case "3":
+                    {
+                        MoarData Ha = new MoarData("half·alive", "Alternative/Indie", "Now, Not Yet, In florescence", "Ice cold, RUNAWAY, Tip toes, What's wrong?, arrow", "Spotify, Youtube Music, Apple Music, Deezer", "\nTwitter: @halfaliveco, Instagram: @halfaliveco, Facebook: @halfalivejoshtaylor");
+                        Console.WriteLine("Artista: {0}\nGenero: {1}\nAlbums: {2}\nCanciones recomendadas: {3}\nLos encontraras en: {4}\nSus redes son: {5}", Ha.NombreA, Ha.Genero, Ha.Albums, Ha.CanRec, Ha.Streaming, Ha.SM);
+                        break;
+                    }
+            }
+
+            
+        }
+    }
+}
